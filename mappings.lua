9c52ba88d8af98227674ba66bab6fd6fab11234e
jakeia <azbazb011@gmail.com>
2024-02-26 22:57:28 +0800

    update

--- mappings.lua
+++ mappings.lua
@@ -24,15 +24,42 @@ return {
     },
     ["<leader>b"] = { name = "Buffers" },
     ["<leader>ft"] = { "<cmd>TodoTelescope<cr>", desc = "Find Todo" },
+    -- markdown preview
     ["<leader>lz"] = { "<cmd>MarkdownPreview<cr>", desc = "Markdown Preview" },
+    -- number increment decrement
     ["+"] = { "<C-a>", desc = "Increment number under cursor" },
     ["-"] = { "<C-x>", desc = "Decrement number under cursor" },
+    -- TSContextToggle
     ["<leader>uz"] = { "<cmd>TSContextToggle<cr>", desc = "Toggle Tscontext" },
+    -- code run
     ["<leader>da"] = {
       "<cmd>let b:caret=winsaveview() <cr> | <cmd>%SnipRun <cr>| <cmd>call winrestview(b:caret) <cr>",
       silent = true,
       desc = "Run Code Without Dap",
     },
+    ["<leader>pd"] = {
+      "<cmd>Neoconf<cr>",
+      desc = "Select Local Or Global Neoconf Config",
+    },
+    ["<leader>pb"] = {
+      "<cmd>Neoconf show<cr>",
+      desc = "Show Neoconf Merge Config",
+    },
+    ["<leader>pc"] = {
+      "<cmd>Neoconf lsp<cr>",
+      desc = "Show Neoconf Merge Lsp Config",
+    },
+    -- ["<leader>m"] = {
+    --   desc = "󰇉test",
+    -- },
+    -- ["<leader>mc"] = {
+    --   function() require("neotest").run.run() end,
+    --   desc = "Run Nearest",
+    -- },
+    -- ["<leader>mC"] = {
+    --   function() require("neotest").run.run() { strategy = "dap" } end,
+    --   desc = "Run Nearest With Dap",
+    -- },
   },
   t = {
     -- setting a mapping to false will disable it
