49d81a0ebbaefde55c8db89f3cb8598b2570babf
jakeia <azbazb011@gmail.com>
2024-03-06 09:10:37 +0800

    neovide update

--- neovide.lua
+++ neovide.lua
@@ -41,11 +41,9 @@ function M.background()
   -- vim.g.neovide_background_color = "#0f1117" .. alpha()
 end
 
--- 设置fps
+--fps config
 function M.refresh(run_fps, free_fps)
-  -- 设置刷新率
   vim.g.neovide_refresh_rate = run_fps
-  -- 空闲刷新率
   vim.g.neovide_refresh_rate_idle = free_fps
 end
 
